cf9249d6a6c1f0c1aae10fee640737bde8e20b7d
Srishti Rajput <rajlutsrishti@gmail.com>
2025-08-09 18:23:17 +0530

    Add server folder which contains backend

---
+++ server/lib/validators.js
@@ -0,0 +1,74 @@
+import {body,validationResult,check,param} from "express-validator"
+import { ErrorHandler } from "../utils/utility.js";
+
+const validateHandler=(req,res,next)=>{
+    const errors=validationResult(req);
+
+    const errorMessages=errors.array().map((error)=>error.msg).join(",");
+
+    console.log(errorMessages);
+
+    if(errors.isEmpty()) return next();
+    else next(new ErrorHandler(errorMessages,400));
+};
+
+const registerValidator=()=>[
+    body("name","Please Enter Name").notEmpty(),
+    body("username","Please Enter Username").notEmpty(),
+    body("password","Please Enter Password").notEmpty(),
+    body("bio","Please Enter Bio").notEmpty(),
+    check("avatar","Please upload avatar"),
+];
+
+const loginValidator=()=>[
+    body("username","Please Enter Username").notEmpty(),
+    body("password","Please Enter Password").notEmpty(),
+];
+
+const newGroupValidator=()=>[
+    body("name","Please Enter Name").notEmpty(),
+
+    body("members")
+    .notEmpty().withMessage("Please Enter Members")
+    .isArray({min:2,max:100}).withMessage("Members must be between 2-100"),
+];
+
+const addMemberValidator=()=>[
+    body("chatId","Please Enter chat ID").notEmpty(),
+
+    body("members")
+    .notEmpty().withMessage("Please Enter Members")
+    .isArray({min:1,max:97}).withMessage("Members must be between 1-97"),
+];
+
+const removeMemberValidator=()=>[
+    body("userId","Please Enter user ID").notEmpty(),
+    body("chatId","Please Enter chat ID").notEmpty(),
+];
+
+const sendAttachmentsValidator=()=>[
+    body("chatId","Please Enter chat ID").notEmpty(),
+     check("files")
+     .notEmpty().withMessage("Please upload attachments")
+    .isArray({min:1,max:5}).withMessage("Attachments must be between 1-5"),
+];
+
+const chatIdValidator=()=>[
+    param("id","Please Enter chat ID").notEmpty(),
+];
+
+const renameGroupValidator=()=>[
+    param("id","Please Enter chat ID").notEmpty(),
+    body("name","Please enter name").notEmpty(),
+];
+
+export {registerValidator,
+    validateHandler,
+    loginValidator,
+    newGroupValidator,
+    addMemberValidator,
+    removeMemberValidator,
+    sendAttachmentsValidator,
+    chatIdValidator,
+    renameGroupValidator,
+};
---
+++ server/middlewares/auth.js
@@ -0,0 +1,15 @@
+import { ErrorHandler } from "../utils/utility.js";
+import jwt from "jsonwebtoken"
+
+ const isAuthenticated=(req,res,next)=>{
+    const token=req.cookies['chattu-token'];
+    if(!token) return next(new ErrorHandler("Please login to access this route",401));
+    const decodedData=jwt.verify(token,process.env.JWT_SECRET);
+    req.user=decodedData._id;
+
+
+    next();
+ };
+
+
+ export {isAuthenticated};
